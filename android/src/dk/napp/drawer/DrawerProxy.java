4859a6d6e7a9b28b0e447b864434d777881dc84c
adamkearsley <djkearsley@hotmail.com>
2015-04-29 09:48:50 +0100

    Revert "Fix for SDK 4.0.0"

--- android/src/dk/napp/drawer/DrawerProxy.java
+++ android/src/dk/napp/drawer/DrawerProxy.java
@@ -135,7 +135,7 @@ public class DrawerProxy extends TiWindowProxy implements TiActivityWindow
 	}
 
 	@Override
-	public void windowCreated(TiBaseActivity activity, Bundle savedInstanceState) {
+	public void windowCreated(TiBaseActivity activity) {
 		slideMenuActivity = new WeakReference<Activity>(activity);
 		activity.setWindowProxy(this);
 		setActivity(activity);
